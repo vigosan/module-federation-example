ecd3772b772d90bc19a31b84780988ac4bade037
Vicent Gozalbes <vigosan@gmail.com>
2021-02-05 08:04:22 +0100

    Make footer fixed on bottom

--- container/src/components/ui/Layout/Layout.js
+++ container/src/components/ui/Layout/Layout.js
@@ -3,9 +3,9 @@ import { Header, Footer, Main } from "../";
 
 function Layout({ children }) {
   return (
-    <div className="container mx-auto bg-white max-w-screen-xl">
-      <div className="h-full max-w-6xl p-20 mx-auto">
-        <div className="flex flex-col h-full space-y-6">
+    <div className="container mx-auto bg-white">
+      <div className="h-full max-w-4xl mx-auto">
+        <div className="flex flex-col h-full min-h-screen py-10">
           <Header />
           <Main>{children}</Main>
           <Footer />
